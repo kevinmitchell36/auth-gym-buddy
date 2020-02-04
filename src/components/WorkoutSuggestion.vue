db125ca76e440b137d4e736336a53eba5a7bac33
Kevin Mitchell <kevin@Kevins-MBP.attlocal.net>
2020-02-03 21:14:24 -0600

    Initial commit

---
+++ src/components/WorkoutSuggestion.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="">
+    <h1>Welcome to Gym Buddy</h1>
+    <h3>What do you want to do today?</h3>
+    <a class="hairy" href="#">Weights</a>
+    <a class="hairy" href="#">Weights</a>
+    <a class="hairy" href="#">Weights</a>
+    <a class="hairy" href="#">Weights</a>
+  </div>
+</template>
+<script>
+export default {
+  name: 'WorkoutSuggestion',
+  components: {
+  }
+}
+</script>
+
+<style>
+
+a.hairy {
+  height: 160px;
+  width: 500px;
+
+  display: block;
+  background-image: url('https://images.unsplash.com/photo-1529516624803-d5e9c2fb067e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80');
+  background-position: center;
+
+}
+
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,14 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <WorkoutSuggestion/>
   </div>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import WorkoutSuggestion from '../components/WorkoutSuggestion.vue';
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    WorkoutSuggestion
   }
 }
-</script>
+</script> 
