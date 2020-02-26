9db4ead6e85170601b4eab275e169c97e15565bd
Kevin Mitchell <kevin@Kevins-MBP.attlocal.net>
2020-02-25 21:36:33 -0600

    Add auth config

--- src/components/partials/Nav.vue
+++ src/components/partials/Nav.vue
@@ -12,14 +12,33 @@
         <router-link to="/" class="navbar-item">Home</router-link>
         <router-link to="/about" class="navbar-item">About</router-link>
       </ul>
-        <button>Sign In</button>
+         <div class="buttons">
+      <!-- Check that the SDK client is not currently loading before accessing is methods -->
+      <div v-if="!$auth.loading">
+        <!-- show login when not authenticated -->
+        <a v-if="!$auth.isAuthenticated" @click="login" class="button"><strong>Sign in</strong></a>
+        <!-- show logout when authenticated -->
+        <a v-if="$auth.isAuthenticated" @click="logout" class="button"><strong>Logout</strong></a>
+
+      </div>
+    </div>
     </div>
   </nav>
 </template>
 
 <script>
 export default {
-    name: 'Nav'
+    name: 'Nav',
+    methods: {
+      login() {
+        this.$auth.loginWithRedirect();
+      },
+      logout() {
+        this.$auth.logout({
+          returnTo: window.location.origin
+        });
+      }
+    }
 }
 </script>
 
